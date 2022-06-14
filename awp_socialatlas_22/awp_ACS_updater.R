5b6081f457fd8360b865fb85a007a31e82f0e9e3
Aidan Hysjulien <61710962+ahysjulien@users.noreply.github.com>
2022-06-14 12:15:11 -0400

    Delete awp_socialatlas_22 directory

--- awp_socialatlas_22/awp_ACS_updater.R
+++
@@ -1,8 +0,0 @@
-# If you have not already done so you must install a unique census api key 
-# which you can obtain from [this page](https://api.census.gov/data/key_signup.html).
-
-# census_api_key("YOUR KEY HERE", install = TRUE) 
-
-source("awp_ACS_fetchdata.R")
-  
-awp_acs_updater(2020)
